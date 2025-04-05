9499b0ab855fe30bb66a2f0588cef264a27130eb
andrewisokoe <andrewisoko1998@gmail.com>
2025-04-05 01:57:02 +0100

    Commit 05/04/2025

--- game_settings.py
+++ game_settings.py
@@ -4,56 +4,122 @@ from teams import Teams
 class GameSettings:
     
     
-    def __init__(self,team:Teams):
-        pass
+    def __init__(self):
+        
+        self.smuggler = None
+        self.inspector = None
+        self.security_amount = None
+        self.game = None
+       
     
+     
+    def the_smuggler(self):
+        
+        """The smuggler's action"""
+        
+        while True: 
+            try:
+
+                self.smuggler = int(input("Place your amount. The max is 100,000,000 £: "))
+                if self.smuggler <= 100_000_000 and self.smuggler.is_integer():
+                    print("Smuggler turn is over")
+                    break  
+                else:
+                    print("Invalid amount. Please try again.")
+            except:
+                print("Invalid input. Please enter a numeric value.")
+       
+    
+    
+    def pass_declaration(self):
+        
+        """All the scenarios occurring from a pass declaration"""
+
+        print(f"Inspector declared PASS, the smuggler carried {self.smuggler:,} £")
+        
+        if self.smuggler == 0:
+            print(f"No money has been smuggled to the outside bank account.")
+            
+        else:
+            print(f"The smuggler has been able to carry {self.smuggler:,} £")
+    
+
+                
+    def doubt_declaration(self):
+        
+        """All the scenarios occurring from a doubt declaration"""
+        
+        while True:
+             
+            try:
+                self.statement_amount = int(input("AMOUNT: "))
+                self.security_amount = self.statement_amount / 2
+                
+                if self.statement_amount < 100_000_000 and self.statement_amount.is_integer():
+                    break
+                else:
+                    print("Invalid amount")
+            except:
+                print("Invalid input. Please enter a numeric value.")
+                
+        if self.smuggler == 0 and self.statement_amount > 0:
+            print(f"Smuggler obtained {self.security_amount:,} £ of security amount.")
+            
+        elif self.statement_amount >= self.smuggler:
+            print(f"Smuggling attempt flopped!!! The inspector obtained {self.smuggler:,} £ into his/her outside country's bank account.")
+                    
+    
+        elif self.statement_amount < self.smuggler:
+            print(f"Smugglers succesfully smuggled {self.smuggler:,} £ on his/her outside country's bank account plus {self.security_amount:,} from the inspector")
+        else:
+            pass
+
+
+                
+    def The_inspector(self):
+        
+        """Inspector's action"""
+        
+        while True:
+            
+            self.inspector = input("insert PASS if you believe the smuggler is carrying no money. Insert DOUBT if you believe money are carried by the smuggler: ")
+            
+            if self.inspector == "PASS":
+                self.pass_declaration()
+                break
+                
+            elif self.inspector == "DOUBT":
+                self.doubt_declaration()
+                break
+            
+            else:
+                print("Invalid input. Please try again")
+                  
+                  
+
+                    
+        
+        
+        
+
     def games(self):
         
         """General game setting"""
         
-        games = 10 #trial
-        
-        while games > 0:
+        for self.game in range(3,0,-1):
             
-            smuggler = int(input("place your amount. the max is 100.000.000 £: "))
-            if smuggler > 100000000:
-                print("Invalid amount")
-            else:
-                print("Smuggler turn is over")
+            self.the_smuggler()
+            self.The_inspector()
+            
+            print(f"{self.game - 1} game(s) remaining.\n")
+        
+ 
+        print("Game Over!")
+    
+           
+  
+           
                 
-            while True:
-                
-                inspector = input("insert PASS if you believe the smuggler is carrying no money. Insert DOUBT if you believe money are carried by the smuggler: ")
-                
-                if inspector == "PASS":
-                    print(f"Inspector declared PASS, the smuggler carried {smuggler} £")
-                    
-                    if smuggler == 0:
-                        print("No money has been smuggled to the outside bank account")
-                        break   
-                    else:
-                        print(f"The smuggler has been able to carry {smuggler} £ into his/her's outside bank account")
-                        break
-                
-                elif inspector == "DOUBT":
-                    statement_amount = int(input("AMOUNT: "))
-                    
-                    if statement_amount > 100000000:
-                        print("Invalid amount")
-                    else:
-                        pass
-                        
-                    security_amount = statement_amount / 2
-                    
-                    if statement_amount >= smuggler:
-                        print(f"Smuggling attempt flopped. The inspector obtained {smuggler} into his/her's bank account")
-                        break
-                    elif smuggler == 0:
-                        print(f"Smuggler obtained {security_amount} £ of security amount")
-                        break
-                    elif statement_amount < smuggler:
-                        print(f"Smugglers succesfully smuggled {smuggler} £ on his/her's outside bank account plus {security_amount} from the inspector")
-                        break
-                else:
-                    print("invalid try again")
-                
+
+game_test = GameSettings()
+game_test.games()
