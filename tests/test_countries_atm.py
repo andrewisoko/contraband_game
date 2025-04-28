fecaf71263ec8a8c93a2a8f9b617ed6431ccb9da
andrewisokoe <andrewisoko1998@gmail.com>
2025-04-28 21:01:02 +0100

    Commit 04/28/2025

--- tests/test_countries_atm.py
+++ tests/test_countries_atm.py
@@ -27,4 +27,21 @@ def test_countries_personal_ba():
         assert united_atm_country_dict[key_names] == 300_000_000
  
    
+  
+def test_smuggling_amount_from_bank():
     
+    test_signups = SignUpProcess()
+    test_teams = Teams(test_signups)
+    test_gamesettings = GameSettings(test_teams)
+    test_banks_atm = Banks(test_signups,test_teams,test_gamesettings)
+    
+    test_banks_atm.northern_atm()
+    test_banks_atm.southern_atm()
+    
+    test_north_atm = test_banks_atm.northern_atm_bankaccounts
+    test_south_atm = test_banks_atm.southern_atm_bankaccounts
+    
+    united_atm_country_dict = test_north_atm
+    united_atm_country_dict.update(test_south_atm)
+    
+
